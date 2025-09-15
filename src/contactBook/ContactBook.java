35cc7a23d19dee4b56071f6df06b7fa2af82ea41
CazuLoureiro <144629494+CazuLoureiro@users.noreply.github.com>
2025-09-15 21:46:51 +0100

    Update ContactBook.java

--- src/contactBook/ContactBook.java
+++ src/contactBook/ContactBook.java
@@ -93,4 +93,15 @@ public class ContactBook {
         return contacts[currentContact++];
     }
 
+     public boolean checkDoubleContacts() {
+    for (int i = 0; i < counter; i++) {
+      for (int j = i + 1; j < counter; j++) {
+        if (contacts[i].getPhone() == contacts[j].getPhone())
+          return true;
+      }
+    }
+    return false;
+  }
+
+
 }
